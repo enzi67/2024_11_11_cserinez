57531fe3d6889e40667c9492ee5b4caf8080e8fe
Cseri Inez <cseri.inez@diak.szbi-pg.hu>
2024-11-11 09:31:59 +0100

    honapok

---
+++ 0_example.py
@@ -0,0 +1,9 @@
+honapok = ["január", "február", "március", "április", "május", "június", "július", "augusztus", "szeptember", "október"]
+
+print(honapok)
+
+# lista hossza: len()
+print(len(honapok))
+
+# 0. indexű elem
+print(honapok[0])
