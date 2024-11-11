b9d1b99b3ae409922d62876ded44ec41c0e6369c
Cseri Inez <cseri.inez@diak.szbi-pg.hu>
2024-11-11 09:36:33 +0100

    szamok

--- 0_example.py
+++ 0_example.py
@@ -10,3 +10,13 @@ print(honapok[0])
 
 # 1. indexű elem
 print(honapok[1])
+
+# üres lista létrehozása
+szamok = []
+print(szamok)
+# elem hozzáadása
+for i in range (1,11):
+    szamok.append(i)        # appenddel hozzáadunk elemeket
+print(szamok)
+
+print(szamok[2])
