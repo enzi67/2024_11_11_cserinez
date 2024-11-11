3ca0ef1c91abd0cbdabebbd5ae4816f66ee31c0b
Cseri Inez <cseri.inez@diak.szbi-pg.hu>
2024-11-11 18:59:23 +0100

    minden

--- 2_2_feladat.py
+++ 2_2_feladat.py
@@ -1,10 +1,6 @@
-"""2.1 Feladat
-Készíts egy programot, amely a felhasználótól kis "a" betűvel kezdődő szavakat kér be és
-ezeket tárolja. Ha a felhasználó nem "a" betűvel kezdődő szót ad meg, akkor azt hagyja figyelmen
-kívül és ne tárolja. A bekérés egészen addig folytatódjon, amíg a felhasználó ENTER-t nem üt
-(nem ad meg újabb nevet a bekérésnél)! A program a bekért neveket írja ki a képernyőre!
-
-A program tájékoztatássa a felhasználót a működéséről, és az elvárt adatokról!"""
+"""2.2 Feladat
+Alakítsd át úgy az előző porgramot, hogy az ne csak kis, hanem a nagy "A" betűvel kezdődő
+szavakat is elfogadja."""
 
 def feladat():
 
@@ -16,7 +12,7 @@ def feladat():
 
     while True:
         szo = input("Adj meg egy szót: ")
-        
+
         if szo == "":
             break
         
