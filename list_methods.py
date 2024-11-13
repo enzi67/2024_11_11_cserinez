bb14c2a41bf3daa4f249eaaf8b7ecdeaf33156e1
Cseri Inez <cseri.inez@diak.szbi-pg.hu>
2024-11-13 14:15:18 +0100

    meszidzs

--- list_methods.py
+++ list_methods.py
@@ -8,3 +8,15 @@ print(nyelvek)
 # fordított sorrendbe rendezi a listát
 nyelvek.reverse()
 print(nyelvek)
+
+
+# az adott elem első előfordulásának indexe
+print(nyelvek.index('C'))
+
+# az adott elem hányszor fordul elő
+print(nyelvek.count('Python'))
+
+# NEM listametódus, de így lehet eldönteni, hogy egy elemet tartalmaz-e a lista
+print('C++' in nyelvek)
+
+  
