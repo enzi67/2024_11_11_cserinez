baca2abf201a78238f391a633c7bd5de838b28ad
Cseri Inez <cseri.inez@diak.szbi-pg.hu>
2024-11-13 14:13:57 +0100

    nyelvek

---
+++ list_methods.py
@@ -0,0 +1,10 @@
+nyelvek = ['Python', 'C', 'C++', 'Java']
+nyelvek2 = sorted(nyelvek)
+
+# sorbarendezi a listát
+nyelvek.sort()
+print(nyelvek)
+
+# fordított sorrendbe rendezi a listát
+nyelvek.reverse()
+print(nyelvek)
