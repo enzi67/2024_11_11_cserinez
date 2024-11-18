4113f198ec2151c358d7fe0262271bb442d173d8
Cseri Inez <cseri.inez@diak.szbi-pg.hu>
2024-11-18 09:15:50 +0100

    kettes

--- 5_1_2_feladat.py
+++ 5_1_2_feladat.py
@@ -2,3 +2,13 @@
 Alakítsuk át az előbbi programot úgy, hogy a program arról adjon tájékoztatást, hogy pontosan hányszor szerepel a felhasználó által
 megadott szín a listában! Ha a megadott szín nincs még tárolva, továbbra is a "A megadott szín nem szerepel a listában." szöveg jelenjen meg!
 """
+
+colours = ['piros', 'narancs', 'citrom', 'zöld', 'kék', 'lila', 'magenta', 'rózsaszín', 'türkiz', 'fekete', 'fehér', 'szürke']
+szin = input("Adj meg egy színt! ")
+
+if szin in colours:
+    print(f"A(z) {szin} {colours.count(szin)} -szer szerepel a listában.")
+else:
+    print(f"A(z) {szin} szín nem szerepel a listában.")
+
+print(f"A lista tartalmazza a {colours} színeket.")
