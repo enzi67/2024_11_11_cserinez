07a5ada301fb0ae123e93624c73d684c80c59a19
Cseri Inez <cseri.inez@diak.szbi-pg.hu>
2024-11-18 09:06:46 +0100

    5os feladatok

---
+++ 5_1_1_feladat.py
@@ -0,0 +1,12 @@
+"""1.1 Feladat
+A program tároljon egy listában színeket. Kérjen be a felhasználótól egy színt, és állapítsa meg,
+hogy a megadott szín már szerepel-e az adott listában. A vizsgálat eredményéről tájékoztassa a program a felhasználót,
+és írja ki egymás mellé vesszővel elválasztva a lista által tartalmazott színeket!"""
+
+colours = ['piros', 'narancs', 'citrom', 'zöld', 'kék', 'lila', 'magenta', 'rózsaszín', 'türkiz', 'fekete', 'fehér', 'szürke']
+input("Adj meg egy színt! ")
+
+
+
+
+print(f"A lista tartalmazza a {colours} színeket.")
---
+++ 5_1_2_feladat.py
@@ -0,0 +1,4 @@
+"""1.2 Feladat
+Alakítsuk át az előbbi programot úgy, hogy a program arról adjon tájékoztatást, hogy pontosan hányszor szerepel a felhasználó által
+megadott szín a listában! Ha a megadott szín nincs még tárolva, továbbra is a "A megadott szín nem szerepel a listában." szöveg jelenjen meg!
+"""
